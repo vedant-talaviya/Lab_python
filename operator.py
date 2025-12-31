86328c6808fe9abfcdfbcb8cea6c6a59df1f27b2
vedant-talaviya <vedanttalavia25@gmail.com>
2025-12-31 08:17:05 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,46 @@
+#1. assignment operator
+x=10
+print(x)
+
+#2. unary minus
+s=5
+print(-s)
+
+#3. relational
+p=34
+w=23
+print(p>w)
+print(p==w)
+
+#4. logical
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#5. boolean
+is_pass=True
+print(is_pass) #o/p=true
+
+#6.bitwise
+a=5
+b=3
+print(a & b)
+print(a | b)
+
+#7. membership
+numbers = [1,2,3,4,6]
+print(2 in numbers) #o/p=true
+print(5 not in numbers) #o/p= false
+
+#8. identity
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+#9. arithmetic operator
+x=10
+y=5
+print(x+y)
