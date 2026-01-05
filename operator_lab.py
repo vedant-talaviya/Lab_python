3fa9dace0fd03b7ecacaeddd210efe27f32fad5d
vedant-talaviya <vedanttalavia25@gmail.com>
2026-01-05 11:17:09 +0530

    Add files via upload

---
+++ operator_lab.py
@@ -0,0 +1,35 @@
+#string
+a="hello world"
+print(a)
+
+#int
+b=18
+print(b)
+
+#boolean
+is_pass=True
+print(is_pass)
+
+#tuple
+c=("BCA","B.Sc.IT","M.Sc.IT")
+print(c)
+
+#list
+d=["MCA","BBA","MBA"]
+print(d)
+
+#dict
+e={"Name": "Vedant","Age":18}
+print(e)
+
+#none
+f=None
+print(f)
+
+#complex
+v=2j
+print(v)
+
+#float 
+r=18.8
+print(r)
