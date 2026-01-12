8f4fc6e92473c5673de5071babcb796b05cbf3d2
vedant-talaviya <vedanttalavia25@gmail.com>
2026-01-12 14:54:34 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,60 @@
+#Q-1
+v="Welcome to Atmiya University"
+print(v)
+
+#Q-2
+r="\tRajesh Kumar \n\tFlat No.101, Sunshine Apartments \n\tMG Road, Sector 15 \n\tRajkot, \n\tPincode:360004 \n\tIndia."
+print(r)
+
+#Q-3
+v=150
+r=120.50
+print(v+r)
+print(v-r)
+print(v*r)
+print(v/r)
+
+#Q-4
+radius=18.5
+area=3.14*radius*radius
+circumference=2*3.14*radius
+print("Area of circle=",area)
+print("Circumference Of Circle=",circumference)
+
+#Q-5
+p=1000
+rate=5
+t=2
+si=(p*rate*t)/100
+print("Simple interest=",si)
+
+#Q-6
+Length=20
+width=10
+perimeter=2*(Length+width)
+print("Perimeter=",perimeter)
+
+#Q-7
+length=20
+width=10
+area_of_rectangle=length+width
+perimeter_of_rectangle=2*(length+width)
+print("Area_of_rectangle=",area_of_rectangle)
+print("Perimeter_of_rectangle=",perimeter_of_rectangle)
+
+#Q-8
+a=2
+b=4
+c=6
+Perimeter_of_triangle=a+b+c
+print("Perimeter of traingle=",Perimeter_of_triangle)
+
+#Q-9
+side=4
+area_of_square=side*side
+print("Area of square=",area_of_square)
+
+#Q-10
+side=4
+perimeter_of_square=4*side
+print("perimeter of square=",perimeter_of_square)
