ffd449b3b7b12a3a2836c9f7fe7dca4fadf0d4c5
Vedant Talaviya <vedanttalavia25@gmail.com>
2026-01-19 11:23:42 +0530

    Add files via upload

---
+++ Task-2.py
@@ -0,0 +1,51 @@
+#1 
+p=1000
+rate=5
+t=2
+si=(p*rate*t)/100
+print("Simple interest=",si)
+
+#2
+v=18
+r=7
+print(max(v,r))
+
+#3
+for i in range(1,6):
+    print(i)
+
+#4
+v="Shivangi"
+print(len(v))
+
+#5
+r="Welcome to Rajkot"
+print(r)
+
+#6
+v="shivangi"
+print(v[0])
+
+#7
+r="shivangi"
+print(r[7])
+
+#8
+number=0
+if number>0:
+    print("positive")
+elif number<0: 
+    print("negative")
+else :
+    print("zero")       
+
+#9
+v=18
+r=2
+s=5
+sum=v+r+s
+print(sum)    
+
+#10
+name=input("Enter your name:")
+print("Hello",name)
