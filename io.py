2344175e17cbf5539198cd73e8a5f4015cf54c1e
Vedant Talaviya <vedanttalavia25@gmail.com>
2026-02-02 11:38:04 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,17 @@
+#int
+s=int(input("Enter Int Numbers:"))
+print("Int Numbers:",s)
+
+#float
+v=float(input("Enter Float Number:"))
+print(" Float Numbers:",v)
+
+#boolean
+value = input("Enter True or False: ")
+print(value)
+
+#string
+name = input("Enter a string: ")
+print("You entered:", name)
+
+
