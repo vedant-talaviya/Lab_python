bc3a7f83b6c8878cc39a86823e44ad73588e2793
Vedant Talaviya <vedanttalavia25@gmail.com>
2026-02-04 09:09:22 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,13 @@
+age=int(input("Enter age:"))
+if age>18:
+    print("you are eligible")
+else:
+    print("You are not eligible")
+
+marks=int(input("Enter marks:"))
+if marks>=90:
+    print("A grade")
+elif marks>=70:
+    print("B grade")
+else:
+    print("C grade")
