dc14962d49657b183465d5eab169186371835ab5
Vedant Talaviya <vedanttalavia25@gmail.com>
2026-02-09 10:08:50 +0530

    Add files via upload

---
+++ statement.py
@@ -0,0 +1,33 @@
+"""age=int(input("Enter Your age:"))
+if age >18:
+    print("Eligible for vote..")
+else:
+    print("Not eligible for vote..")
+
+marks=int(input("Enter marks:"))
+if marks>=90:
+    print("A grade")
+elif marks>=70:
+    print("B grade")
+else:
+    print("C grade")
+
+number=int(input("Enter number:"))
+if number%2==0:
+    print("Even")
+else:
+    print("Odd")
+
+number=int(input("Enter number:"))
+if number>0:
+    print("Positive")
+elif number<0:
+    print("Negative")
+else:
+    print("Zero")"""
+
+age=int(input("Enter age:"))
+if age>18:
+    print("you are eligible for licence")
+else:
+    print("You are not eligible for licence")
